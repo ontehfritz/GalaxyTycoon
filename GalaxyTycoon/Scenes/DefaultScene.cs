8a2e39c6159f626b869dbdd15de4f379cc2d6e8d
Fritz (Fredrick Seitz) <fredrick.seitz@gmail.com>
2018-11-06 18:56:29 -0500

    Added sprite

--- GalaxyTycoon/Scenes/DefaultScene.cs
+++ GalaxyTycoon/Scenes/DefaultScene.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using GalaxyTycoon.Components;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Nez;
+using Nez.Pipeline;
+using Nez.Sprites;
 
 namespace GalaxyTycoon.Scenes
 {
@@ -18,11 +21,14 @@ namespace GalaxyTycoon.Scenes
                                Scene.SceneResolutionPolicy.ShowAllPixelPerfect);
             Screen.setSize(137 * 9, 89 * 9);
 
+            var texture = this.content.Load<Texture2D>("Bee");
             var mouseFollowEntity = createEntity("mouse-follow");
             mouseFollowEntity.addComponent(new MouseEntity());
             mouseFollowEntity.addComponent(new CameraBounds(
                 new Vector2(16, 16), new Vector2(100, 100)));
             camera.entity.addComponent(new FollowCamera(mouseFollowEntity));
+            mouseFollowEntity.addComponent(new Sprite(texture));
+
         }
     }
 }
